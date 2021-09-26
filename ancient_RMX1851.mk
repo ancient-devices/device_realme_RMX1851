9e12b44a306919c06cf2e40c7fd024900be5059b
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-09-26 16:03:55 +0530

    RMX1851: Ancient Bringup

--- ancient_RMX1851.mk
+++ ancient_RMX1851.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AncientOS stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -13,9 +13,12 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
+# Ancient
+ANCIENT_OFFICIAL := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1851
-PRODUCT_NAME := aosp_RMX1851
+PRODUCT_NAME := ancient_RMX1851
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_MANUFACTURER := Realme
